98a05fb5d9fb2000131cd180b5073413576ed4ee
이동건 <developer@idong-geon-ui-MacBookPro.local>
2023-03-10 20:40:13 +0900

    feat: 상품 카드 및 목록 구현
    데이터를 사용하여 여행 상품 카드와 목록을 구현하였습니다.
    데이터를 fetch하여 리덕스 store에 저장함.
    
    Co-authored-by: Park Su Been <bymine@users.noreply.github.com>
    Co-authored-by: 이춘구 <Chun-gu@users.noreply.github.com>
    Co-authored-by: theo-jin <theo-jin@users.noreply.github.com>

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,3 +1,25 @@
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import ProductList from '../components/ProductList';
+import { fetchProductData } from '../store/slices/productSlice';
+
 export default function MainPage() {
-  return <div>Main</div>;
+  const products = useSelector((state) => state.product.products);
+  const dispatch = useDispatch();
+
+  console.log(products);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('./product_list.json');
+      const data = await response.json();
+      dispatch(fetchProductData(data));
+    }
+    fetchData();
+    if (!products.length) fetchData();
+  }, []);
+
+  return <ProductList products={products}></ProductList>;
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,12 @@
 // store.js
+import { configureStore } from '@reduxjs/toolkit';
+
+import { productReducer } from './slices';
+
+const store = configureStore({
+  reducer: {
+    product: productReducer,
+  },
+});
+
+export default store;
