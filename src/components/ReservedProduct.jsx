b1df9c34a15a0fdb0b389fc42a9eeb263934879a
Jimmy <qny123@naver.com>
2023-03-10 23:14:05 +0900

    feat: 상품 리스트 & 상품 컴포넌트 구현
    상품 리스트 & 상품 컴포넌트 구현했습니다.
    
    관련 이슈: #9

---
+++ src/components/ReservedProduct.jsx
@@ -0,0 +1,87 @@
+import {
+  Box,
+  Td,
+  Tr,
+  Image,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Center,
+} from '@chakra-ui/react';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { reservationActions } from '../store/slices/reservationSlice';
+
+export default function ReservedProduct({ product }) {
+  const dispatch = useDispatch();
+
+  const reservedProducts = useSelector(
+    (state) => state.reserve.reservedProducts,
+  );
+  const quantity = reservedProducts.filter(
+    (item) => item.idx === product.idx,
+  )[0].quantity;
+  console.log(11, quantity);
+  return (
+    <Tr key={product.idx}>
+      <Td>
+        <Box width="60px">
+          <Image src={product.mainImage} alt={product.name} rounded="lg" />
+        </Box>
+      </Td>
+      <Td>{product.name}</Td>
+      <Td>₩{product.price.toLocaleString()}</Td>
+      <Td>
+        <NumberInput
+          size="md"
+          maxW={24}
+          defaultValue={1}
+          min={1}
+          max={product.maximumPurchases}>
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper
+              onClick={() =>
+                dispatch(
+                  reservationActions.changeQuantity({
+                    idx: product.idx,
+                    newQuantity: 1,
+                  }),
+                )
+              }
+            />
+            <NumberDecrementStepper
+              onClick={() =>
+                dispatch(
+                  reservationActions.changeQuantity({
+                    idx: product.idx,
+                    newQuantity: -1,
+                  }),
+                )
+              }
+            />
+          </NumberInputStepper>
+        </NumberInput>
+      </Td>
+      <Td>
+        <Box as="span" mr={2}>
+          ₩{(product.price * (quantity ? quantity : 1)).toLocaleString()}
+        </Box>
+      </Td>
+      <Td>
+        <Center
+          as="span"
+          mr={2}
+          cursor="pointer"
+          onClick={() =>
+            dispatch(reservationActions.removeFromCart(product.idx))
+          }>
+          <RiDeleteBin6Line color="red.500" />
+        </Center>
+      </Td>
+    </Tr>
+  );
+}
--- src/components/ReservedProductList.jsx
+++ src/components/ReservedProductList.jsx
@@ -1,35 +1,19 @@
-import {
-  CheckIcon,
-  DeleteIcon,
-  ChevronUpIcon,
-  ChevronDownIcon,
-} from '@chakra-ui/icons';
-import {
-  Box,
-  Button,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  chakra,
-  Text,
-  Center,
-  Image,
-} from '@chakra-ui/react';
+import { Box, Table, Thead, Tbody, Th, Center, Text } from '@chakra-ui/react';
+import { FiShoppingCart } from 'react-icons/fi';
+
+import ReservedProduct from './ReservedProduct';
 
 export default function ReservedProductList({ reservedProducts }) {
   return (
     <Box p={8}>
-      <chakra.h1
+      <Center
         fontSize="33px"
-        mt={10}
-        pb={20}
+        pb={15}
         borderBottom="3px solid #ff7373"
-        mb={15}>
-        Check your Pay List
-      </chakra.h1>
+        display="flex">
+        <FiShoppingCart />
+        <Text ml={2}>장바구니</Text>
+      </Center>
       <Table variant="striped" colorScheme="gray.200" w="100%">
         <colgroup>
           <col width="10%" />
@@ -42,55 +26,20 @@ export default function ReservedProductList({ reservedProducts }) {
         <Thead borderBottom="3px solid #ff7373">
           <tr>
             <Th />
-            <Th />
             <Th>상품</Th>
             <Th>판매 가격</Th>
-            <Th pl="38px">수량</Th>
-            <Th pl="38px">결제 금액</Th>
+            <Th>수량</Th>
+            <Th>결제 금액</Th>
             <Th />
           </tr>
         </Thead>
         <Tbody fontSize="xs">
           {reservedProducts.map((product) => (
-            <Tr key={product.idx}>
-              <Td>
-                <Box as="span" mr={2}>
-                  <CheckIcon color="green.500" />
-                </Box>
-              </Td>
-              <Td>
-                <Box width="60px">
-                  <Image
-                    src={product.mainImage}
-                    alt={product.name}
-                    rounded="lg"
-                  />
-                </Box>
-              </Td>
-              <Td>{product.name}</Td>
-              <Td>₩{product.price.toLocaleString()}</Td>
-              <Td>
-                <Box display="flex">
-                  <Button size="sm" mr={2}>
-                    <ChevronDownIcon />
-                  </Button>
-                  <Center fontSize="xs">1</Center>
-                  <Button size="sm" ml={2}>
-                    <ChevronUpIcon />
-                  </Button>
-                </Box>
-              </Td>
-
-              <Td>
-                <Box as="span" mr={2} cursor="pointer">
-                  <DeleteIcon color="red.500" />
-                </Box>
-              </Td>
-            </Tr>
+            <ReservedProduct product={product} />
           ))}
         </Tbody>
       </Table>
-      <Box
+      {/* <Box
         borderTop="4px solid gray"
         borderBottom="4px solid gray"
         p={4}
@@ -107,90 +56,7 @@ export default function ReservedProductList({ reservedProducts }) {
         <Button variant="solid" colorScheme="blue" size="lg">
           make a payment
         </Button>
-      </Box>
+      </Box> */}
     </Box>
   );
 }
-
-/*
-const ReservationsPage = () => {
-  const dispatch = useDispatch();
-  const reservations = useSelector((state) => state.reservations);
-  const content = useSelector((state) => state.contents);
-
-  return (
-    <>
-      <Link to="/main">메인으로</Link>
-      <S.TableContainer margin="200px">
-        <S.Table variant="striped" colorScheme="blue">
-          <S.TableCaption placement="top">예약 내역 조회</S.TableCaption>
-          <S.Thead>
-            <S.Tr>
-              <S.Th>상품 번호</S.Th>
-              <S.Th>상품 이름</S.Th>
-              <S.Th>수량</S.Th>
-              <S.Th>금액</S.Th>
-              <S.Th>삭제</S.Th>
-            </S.Tr>
-          </S.Thead>
-          <S.Tbody>
-            {reservations?.map((item) => (
-              <S.Tr key={item.id}>
-                <S.Td>{item.id}</S.Td>
-                <S.Td>{content[item.id].name}</S.Td>
-                <S.Td>
-                  <S.NumberInput
-                    size="sm"
-                    maxW={20}
-                    value={item.currentPurchases}
-                    min={1}
-                    max={content[item.id].maximumPurchases}
-                  >
-                    <S.NumberInputField readOnly />
-                    <S.NumberInputStepper>
-                      <S.NumberIncrementStepper
-                        onClick={() => {
-                          dispatch(
-                            addcurrentPurchases({
-                              id: item.id,
-                              max: content[item.id].maximumPurchases,
-                            })
-                          );
-                        }}
-                      />
-                      <S.NumberDecrementStepper
-                        onClick={() => {
-                          dispatch(subtractcurrentPurchases(item.id));
-                        }}
-                      />
-                    </S.NumberInputStepper>
-                  </S.NumberInput>
-                </S.Td>
-                <S.Td>{content[item.id].price}</S.Td>
-                <S.Td>
-                  <S.Button
-                    onClick={() => {
-                      dispatch(deleteReservationsList(item.id));
-                      console.log(reservations);
-                    }}
-                  >
-                    삭제
-                  </S.Button>
-                </S.Td>
-              </S.Tr>
-            ))}
-          </S.Tbody>
-          <S.Tfoot>
-            <S.Tr>
-              <S.Th>{''}</S.Th>
-              <S.Th>{''}</S.Th>
-              <S.Th>{''}</S.Th>
-              <S.Th>총 금액</S.Th>
-              <S.Th>{''}</S.Th>
-            </S.Tr>
-          </S.Tfoot>
-        </S.Table>
-      </S.TableContainer>
-    </>
-  );
-}; */
