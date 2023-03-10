1996a1d3cd677ee4b11971655112f682a0f0d488
Jimmy <qny123@naver.com>
2023-03-10 23:15:56 +0900

    feat : redux toolit store 설정
    reservation slice 설정 및 reducer 구현했습니다.
    
    관련 이슈 #9

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,11 @@
-// store.js
+import { configureStore } from '@reduxjs/toolkit';
+
+import reserveReducer from './slices/reservationSlice';
+
+const store = configureStore({
+  reducer: {
+    reserve: reserveReducer,
+  },
+});
+
+export default store;
