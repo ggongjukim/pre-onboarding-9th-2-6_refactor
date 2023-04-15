016366d6a4b9b7e71373a9d4cc96a5248a101ec9
ggongjukim <kch7892003@gmail.com>
2023-04-15 17:21:07 +0900

    refactor: 카드 크기 조절

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -39,7 +39,7 @@ export default function ProductCard({ product }) {
       rounded="lg"
       shadow="lg"
       position="relative"
-      w="100%"
+      w="400px"
       h="500px">
       <Image
         src={product.mainImage}
